07f22186029311004435f9ebb23eb5f405e5b038
soet02 <alswnsrlf12@naver.com>
2025-06-24 09:16:55 +0900

    docs: config 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: './',  // 확장 프로그램에서는 상대 경로를 사용해야 하므로 './'로 설정
   build: {
-    outDir: 'dist', // 빌드 결과물의 출력 경로 설정
     rollupOptions: {
-      input: 'index.html'  // 빌드에 사용할 HTML 파일 설정
-    }
-  }
+      input: {
+        // 각 스크립트 파일 및 HTML 파일을 빌드 입력으로 설정
+        popup: resolve(__dirname, 'popup.html'),
+        content: resolve(__dirname, 'src/content.js'),
+        controller: resolve(__dirname, 'src/controller.js'),
+      },
+      output: {
+        entryFileNames: '[name].js',  // 각 entry 파일에 대해 파일 이름 그대로 사용
+        chunkFileNames: '[name]-[hash].js',  // 청크 파일 이름 지정
+        assetFileNames: '[name]-[hash][extname]',  // 정적 자산 파일 이름 지정
+      }
+    },
+    outDir: 'dist',  // 빌드된 결과물이 저장될 폴더
+    emptyOutDir: true,  // 빌드 시 기존 dist 폴더를 비우고 새로 덮어씌움
+  },
+  publicDir: 'public',  // manifest.json, 아이콘 등 public 폴더에 있는 정적 파일을 dist 폴더로 복사
 });
